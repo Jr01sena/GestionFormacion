271bdf3b0abc510775643b88e49952d3e9603b3b
Jorge Andres <jr01sena@gmail.com>
2025-05-26 17:18:27 -0500

    Actualizar main.py para incluir rutas de ambiente_formacion

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, users
-
+from app.api import ambiente
 
 app = FastAPI()
 
 # Incluir en el objeto app los routers
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(auth.router, prefix="/access", tags=["login"])
+app.include_router(ambiente.router, prefix="/ambiente", tags=["Ambiente Formación"])
+
 
 # Configuración de CORS para permitir todas las solicitudes desde cualquier origen
 app.add_middleware(
