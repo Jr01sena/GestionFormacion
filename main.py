278f9a2665864ce3f8715cd33a442ed025a38fc2
Jorge Andres <jr01sena@gmail.com>
2025-06-03 17:58:48 -0500

    Agregando el archivo cargar_archivos en la carpeta crud y api. Subiendo carga masivo de archivos de excel a la base de datos

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, users
 from app.api import ambiente
+from app.api import cargar_archivos
 
 app = FastAPI()
 
 # Incluir en el objeto app los routers
 app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(auth.router, prefix="/access", tags=["login"])
-app.include_router(ambiente.router, prefix="/ambiente", tags=["Ambiente Formación"])
-
+app.include_router(ambiente.router, prefix="/ambiente", tags=["Ambiente formación"])
+app.include_router(cargar_archivos.router, prefix="/archivos", tags=["Cargar archivos"])
 
 # Configuración de CORS para permitir todas las solicitudes desde cualquier origen
 app.add_middleware(
